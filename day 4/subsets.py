0af95a9b617efc661f816cd91a92fd08fa8074f1
guru mahesh reddy <132872700+Mahesh93811@users.noreply.github.com>
2024-08-30 12:40:30 +0530

    Delete day 4 directory

--- day 4/subsets.py
+++
@@ -1,8 +0,0 @@
-def subsets(nums):
-    result = [[]] 
-    
-    for num in nums:
-        new_subsets = [curr + [num] for curr in result]
-        result.extend(new_subsets)
-    
-    return result
